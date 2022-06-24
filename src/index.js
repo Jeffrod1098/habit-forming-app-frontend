976de90b951d3ce1f947e261cea232633e3555aa
Jeff <mrjeffrey1011@gmail.com>
2022-06-24 14:02:32 -0400

    home/landing page was created

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import purecss from "purecss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
