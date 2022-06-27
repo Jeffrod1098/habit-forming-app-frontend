1ede53fa6b33dbd1ccbdcb0725ecdb120c868b5c
Oktay esen <oktayesenn@gmail.com>
2022-06-27 16:08:11 -0400

    Created Habit Page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import CreateUser from "./CreateUser";
 import axios from "axios";
 import CreateHabit from "./CreateHabit";
 import Login from "./Login";
+import "./Createhabit.css";
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       {/* <Landing /> */}
       {/* <CreateUser /> */}
-      {/* <CreateHabit /> */}
-      <Login />
+      <CreateHabit />
+      {/* <Login /> */}
     </div>
   );
 }
--- src/CreateHabit.js
+++ src/CreateHabit.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios'
 import { useState } from 'react'
+import habitImg from "./image/habits.jpg"
 
 
 const CreateHabit = () => {
@@ -16,6 +17,12 @@ const CreateHabit = () => {
             habitCategory: habitCategory
         })
     }
+    // const backgroundImageStyle = {
+    //     backgroundImage: `url("${habitImg}")`,
+    //     backgroundSize: "cover",
+    //     width: "100vw",
+    //     height: "100vh",
+    // }
 
     const handleHabitNameChange = event => {
         setHabitName(event.target.value)
@@ -26,9 +33,32 @@ const CreateHabit = () => {
     const handleHabitCategoryChange = event => {
         setHabitCategory(event.target.value)
     }
+    const handleSubmit = event => {
+        event.preventDefault()
+        click()
+    }
+    const backgroundImageStyle = {
+        backgroundImage: `url("${habitImg}")`,
+        backgroundSize: "cover",
+        width: "100vw",
+    }
 
     return (
-        <div className="habits" >
+        
+    <div style={backgroundImageStyle}>
+        <div className='nav'>
+                    <div className="logo">
+                        <h3 className='text-3xl text-slate-700'>MyHabits</h3>
+                    </div>
+                    <nav class="flex justify-center space-x-4">
+                        <a href="/home" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+                        <a href="/team" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
+                        <a href="/login" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</a>
+                        <a href="/explore" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Explore</a>
+                    </nav>
+                </div>
+        <div className='App' >
+            <h1 className="today">Today's habits/goals</h1>
             <form onSubmit={click}>
             <input onChange={handleHabitNameChange} type="name" class="form-control block
         w-full
@@ -73,11 +103,12 @@ const CreateHabit = () => {
         transition
         ease-in-out
         m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput127"
         placeholder="Habit Description" />
-            <button onClick={click} class="btn btn-primary">Create</button>
+          <button onClick={handleSubmit} type="submit" class="createbtn">Create a habit</button>
             </form>
         </div>
+    </div>
     )
 }
 
