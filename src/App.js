9addc15d9dcd4597d00a3eb50ef1946587473636
Jeff <mrjeffrey1011@gmail.com>
2022-06-27 11:31:57 -0400

    added forms page to front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Landing from "./Landing";
+import CreateUser from "./CreateUser";
+import axios from "axios"
 
 function App() {
+
+
+
   return <div className="App">
-<Landing />
+    {/* <Landing /> */}
+    <CreateUser />
   </div>;
 }
 
