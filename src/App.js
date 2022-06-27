0e06856681763bac99a4f38b9fee32600facc689
Brian Schmeling <brianschmeling2@gmail.com>
2022-06-27 12:19:17 -0400

    Fix Merge conflicts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Landing from "./Landing";
 import CreateUser from "./CreateUser";
 import axios from "axios";
 import CreateHabit from "./CreateHabit";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
       {/* <Landing /> */}
       {/* <CreateUser /> */}
-      <CreateHabit />
+      {/* <CreateHabit /> */}
+      <Login />
     </div>
   );
 }
