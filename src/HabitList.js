af99fc5b968e9cac7e3ee026362facba8fd51d7a
Jeff <mrjeffrey1011@gmail.com>
2022-06-28 09:53:06 -0400

    added card for each habit listed for habitList page.

--- src/HabitList.js
+++ src/HabitList.js
@@ -39,6 +39,36 @@ const HabitList = () => {
                     )
                 })
             } */}
+
+<div class="flex justify-center">
+  <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+    <div class="py-3 px-6 border-b border-gray-300">
+      Habit Name
+    </div>
+    <div class="p-6">
+      <p class="text-gray-700 text-base mb-4">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <button type="button" class="  w-full
+  px-6
+  py-2.5
+  bg-red-600
+  text-white
+  font-medium
+  text-xs
+  leading-tight
+  uppercase
+  rounded
+  shadow-md
+  hover:bg-red-700 hover:shadow-lg
+  focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
+  active:bg-red-800 active:shadow-lg
+  transition
+  duration-150
+  ease-in-out">Delete</button>
+    </div>
+  </div>
+</div>
         </div>
     )
 }
