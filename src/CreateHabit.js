47b31b8d64bc8dd32671a3a15160bbf3a69f76d2
Jeff <mrjeffrey1011@gmail.com>
2022-06-28 09:25:23 -0400

    add radio buttons to createHabits page and fixed routing bugs

--- src/CreateHabit.js
+++ src/CreateHabit.js
@@ -5,17 +5,17 @@ import habitImg from "./image/habits.jpg"
 import Nav from "./Nav";
 
 
+
 const CreateHabit = () => {
 
     const [habitName, setHabitName] = useState({})
-    const [habitDescription, setHabitDescription] = useState({})
-    const [habitCategory, setHabitCategory] = useState({})
+    const [goal, setGoal] = useState({})
+
 
     const click = () => {
         axios.post('http://localhost:4000/',{
             habitName: habitName,
-            habitDescription: habitDescription,
-            habitCategory: habitCategory
+            goal: goal,
         })
     }
     // const backgroundImageStyle = {
@@ -28,11 +28,8 @@ const CreateHabit = () => {
     const handleHabitNameChange = event => {
         setHabitName(event.target.value)
     }
-    const handleHabitDescriptionChange = event => {
-        setHabitDescription(event.target.value)
-    }
-    const handleHabitCategoryChange = event => {
-        setHabitCategory(event.target.value)
+    const handleGoalChange = event => {
+        setGoal(event.target.value)
     }
     const handleSubmit = event => {
         event.preventDefault()
@@ -72,7 +69,7 @@ const CreateHabit = () => {
                         placeholder="Habit Name" />
                 </div>
                 <div className="form-group mb-6">
-                    <input onChange={handleHabitDescriptionChange} type="text" className="form-control block
+                    <input onChange={handleGoalChange} type="text" className="form-control block
     w-full
     px-3
     py-1.5
@@ -88,6 +85,37 @@ const CreateHabit = () => {
     focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
                         placeholder="Brief Description of Habit" />
                 </div>
+
+                <div class="flex justify-center">
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio10">Mon</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Tue</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Wed</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Thu</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Fri</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Sat</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+    <label class="form-check-label inline-block text-gray-800" for="inlineRadio20">Sun</label>
+  </div>
+</div>
                 
                 <button type="submit" className="
   w-full
