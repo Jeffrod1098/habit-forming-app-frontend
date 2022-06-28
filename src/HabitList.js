d2ad5568e84a7e94d01512b140ba5b8bb59b665f
Jeff <mrjeffrey1011@gmail.com>
2022-06-28 11:03:54 -0400

    made axios get request for habitList page, renders the cards for each seeded habit data

--- src/HabitList.js
+++ src/HabitList.js
@@ -9,25 +9,61 @@ import habitImg from "./image/habits.jpg"
 
 const HabitList = () => {
 
-        const backgroundImageStyle = {
-            backgroundImage: `url("${habitImg}")`,
-            backgroundSize: "cover",
-            width: "100vw",
+    const backgroundImageStyle = {
+        backgroundImage: `url("${habitImg}")`,
+        backgroundSize: "cover",
+        width: "100vw",
+    }
+
+    const [habits,setHabits]= useState([])
+
+    React.useEffect(() => {
+        async function getHabits(){
+            const response = await axios.get('http://localhost:4000/habits')
+            console.log(response.data)
+            setHabits(response.data)
         }
+        getHabits()
+    },[])
+    
+    {habits.map((habit) => {
+        return (
+            <div class="flex justify-center">
+                <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+                    <div class="py-3 px-6 border-b border-gray-300">
+                        {habit.className}
+                    </div>
+                    <div class="p-6">
+                        <p class="text-gray-700 text-base mb-4">
+                            {habit.description}
+                        </p>
+                        <button type="button" class="  w-full
+px-6
+py-2.5
+bg-red-600
+text-white
+font-medium
+text-xs
+leading-tight
+uppercase
+rounded
+shadow-md
+hover:bg-red-700 hover:shadow-lg
+focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
+active:bg-red-800 active:shadow-lg
+transition
+duration-150
+ease-in-out">Delete</button>
+                    </div>
+                </div>
+            </div>
+        )
+    })}
 
-    // const [habits,SetHabits]= ({})
 
-    // React.useEffect(() => {
-    //     async function getHabits(){
-    //         const response = await axios.get('http://localhost:4000/habits')
-    //         SetHabits(response.data)
-    //     }
-    //     getHabits()
-    // },[])
-
-    return(
+    return (
         <div className='App' style={backgroundImageStyle}>
-            <Nav/>
+            <Nav />
             {/* {
                 habits.map((habit) => {
                     return(
@@ -40,7 +76,7 @@ const HabitList = () => {
                 })
             } */}
 
-<div class="flex justify-center">
+            {/* <div class="flex justify-center">
   <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
     <div class="py-3 px-6 border-b border-gray-300">
       Habit Name
@@ -68,7 +104,42 @@ const HabitList = () => {
   ease-in-out">Delete</button>
     </div>
   </div>
-</div>
+</div> */}
+
+{habits.map((habit) => {
+        return (
+            <div class="flex justify-center">
+                <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+                    <div class="py-3 px-6 border-b border-gray-300">
+                        {habit.name}
+                    </div>
+                    <div class="p-6">
+                        <p class="text-gray-700 text-base mb-4">
+                            {habit.description}
+                        </p>
+                        <button type="button" class="  w-full
+px-6
+py-2.5
+bg-red-600
+text-white
+font-medium
+text-xs
+leading-tight
+uppercase
+rounded
+shadow-md
+hover:bg-red-700 hover:shadow-lg
+focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
+active:bg-red-800 active:shadow-lg
+transition
+duration-150
+ease-in-out">Delete</button>
+                    </div>
+                </div>
+            </div>
+        )
+    })}
+
         </div>
     )
 }
