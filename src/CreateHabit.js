ae4e0c48f6edb5c012b970f5cbebc2701c20c833
Jeff <mrjeffrey1011@gmail.com>
2022-06-28 09:08:07 -0400

    create habit page was made using tail wind

--- src/CreateHabit.js
+++ src/CreateHabit.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from 'axios'
 import { useState } from 'react'
 import habitImg from "./image/habits.jpg"
+import Nav from "./Nav";
 
 
 const CreateHabit = () => {
@@ -46,67 +47,69 @@ const CreateHabit = () => {
     return (
         
     <div style={backgroundImageStyle}>
-        <div className='nav'>
-                    <div className="logo">
-                        <h3 className='text-3xl text-slate-700'>MyHabits</h3>
-                    </div>
-                    <nav class="flex justify-center space-x-4">
-                        <a href="/home" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
-                        <a href="/team" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
-                        <a href="/login" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</a>
-                        <a href="/explore" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Explore</a>
-                    </nav>
-                </div>
+        <Nav />
+        {/* NavBar */}
         <div className='App' >
-            <h1 className="today">Today's habits/goals</h1>
+        <h1 className="text-5xl text-slate-700 font-bold"> Create a MyHabits</h1>
+        {/* Heading */}
+        <div className="form block p-6 rounded-lg shadow-lg bg-white max-w-md">
             <form onSubmit={click}>
-            <input onChange={handleHabitNameChange} type="name" class="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
-        placeholder="Habit Name" />
-            <input onChange={handleHabitDescriptionChange} type="Habit Category" class="form-control block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
-        placeholder="Habit Category" />
-            <input onChange={handleHabitCategoryChange} type="Habit Description" class="form-control block
-        w-full
-        px-3
-        py-1.5 
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput127"
-        placeholder="Habit Description" />
-          <button onClick={handleSubmit} type="submit" class="createbtn">Create a habit</button>
+                <div className="form-group mb-6">
+                    <input onChange={handleHabitNameChange} type="text" className="form-control block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput125"
+                        placeholder="Habit Name" />
+                </div>
+                <div className="form-group mb-6">
+                    <input onChange={handleHabitDescriptionChange} type="text" className="form-control block
+    w-full
+    px-3
+    py-1.5
+    text-base
+    font-normal
+    text-gray-700
+    bg-white bg-clip-padding
+    border border-solid border-gray-300
+    rounded
+    transition
+    ease-in-out
+    m-0
+    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput126"
+                        placeholder="Brief Description of Habit" />
+                </div>
+                
+                <button type="submit" className="
+  w-full
+  px-6
+  py-2.5
+  bg-blue-600
+  text-white
+  font-medium
+  text-xs
+  leading-tight
+  uppercase
+  rounded
+  shadow-md
+  hover:bg-blue-700 hover:shadow-lg
+  focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+  active:bg-blue-800 active:shadow-lg
+  transition
+  duration-150
+  ease-in-out">Create</button>
             </form>
+        </div>
+  
         </div>
     </div>
     )
--- src/Nav.js
+++ src/Nav.js
@@ -9,17 +9,17 @@ const Nav = () =>{
             <h3 className='text-3xl text-slate-700'>MyHabits</h3>
         </div>
         <nav className="flex justify-center space-x-4">
-            <Link to= "/">
-                <a  className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+            <Link to= "/" >
+                <a className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
             </Link>
             <Link to="/createHabit">
                 <a className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Create Habit</a>
             </Link>
-            <Link to="login">
+            <Link to="/login">
                 <a className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</a>
             </Link>
             <Link to="/habitList">
-                <a className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Explore</a>
+                <a className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Habits</a>
             </Link>
         </nav>
     </div>
