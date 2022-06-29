f8117e8226ef8492efc83e6aec1fdacc20b244ba
Brian Schmeling <brianschmeling2@gmail.com>
2022-06-29 10:13:13 -0400

    Update classes to remove errors

--- src/CreateHabit.js
+++ src/CreateHabit.js
@@ -101,107 +101,107 @@ const CreateHabit = () => {
               />
             </div>
 
-            <div class="flex justify-center">
-              <div class="form-check form-check-inline">
+            <div className="flex justify-center">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio1"
                   value="option1"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio10"
                 >
                   Mon
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Tue
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Wed
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Thu
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Fri
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Sat
                 </label>
               </div>
-              <div class="form-check form-check-inline">
+              <div className="form-check form-check-inline">
                 <input
-                  class="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
                   name="inlineRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label
-                  class="form-check-label inline-block text-gray-800"
+                  className="form-check-label inline-block text-gray-800"
                   for="inlineRadio20"
                 >
                   Sun
